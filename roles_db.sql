0da823b3e4800f49d5e5147e4c7df5d03c5e4a25
Mateo Ferreira <matteo948@icloud.com>
2021-02-06 00:29:53 -0500

    README.md--script.js

---
+++ roles_db.sql
@@ -0,0 +1,24 @@
+
+--Drops the database if it exists currently--
+DROP DATABASE IF EXISTS roles_db;
+-- Creates the 'role_db' database --
+CREATE DATABASE roles_db;
+-- make it so all the following code will affect role_db --
+USE roles_db;
+-- Creates the table 'role' within roles_db --
+CREATE TABLE role (
+    id INTEGER(2) AUTO_INCREMENT NOT NULL,
+    title VARCHAR (30),
+    salary DECIMAL
+)
+-- Creates new rows containing data in all named columns --
+INSERT INTO role (title,salary)
+VALUES ("WEB DEV",73000);
+
+-- Updates the row where the column name is peter --
+UPDATE role
+SET has_pet = true, pet_name = "Franklin", pet_age = 2
+WHERE id = 4;
+
+
+SELECT * FROM role;
